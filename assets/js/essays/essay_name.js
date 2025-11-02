a74de8c6a4d5cbc974f86c1cecf955559f6c03a3
aawsomgames <142371120+aawsomgames@users.noreply.github.com>
2025-11-01 20:44:25 -0500

    123

--- assets/js/essays/essay_name.js
+++ assets/js/essays/essay_name.js
@@ -2,15 +2,15 @@
 const ESSAYS = [
   {
     id: "1",
-    title: "Why Morning Matters",
-    description: "Exploring the importance of routines and early habits.",
-    createdAt: "2023-01-15T10:00:00Z",
+    title: "Introduction Paper",
+    description: "A paper introducing myself, my goals, and what I hope learn in ENG 104 at Jackson State",
+    createdAt: "2025-08-25T22:30:00Z",
   },
   {
     id: "2",
     title: "Building Structures",
     description: "How architecture influences our daily lives.",
-    createdAt: "2023-05-20T15:30:00Z",
+    createdAt: "2025-08-30T00:00:01Z",
   },
   {
     id: "3",
@@ -18,4 +18,22 @@ const ESSAYS = [
     description: "A thoughtful look at the lessons from today.",
     createdAt: "2022-09-10T18:45:00Z",
   },
+   {
+    id: "4",
+    title: "Evening Reflections",
+    description: "A thoughtful look at the lessons from today.",
+    createdAt: "2022-09-10T18:45:00Z",
+  },
+   {
+    id: "5",
+    title: "Evening Reflections",
+    description: "A thoughtful look at the lessons from today.",
+    createdAt: "2022-09-10T18:45:00Z",
+  },
+  /* {
+    id: "#",
+    title: "TITLE NAME",
+    description: "A DESCRIPTION",
+    createdAt: "YYYY-MM-DDT18:45:00Z",
+  }, */
 ];
